63a865476f086e3a26f0bf46434b6e7ebb45aa16
James <143594473+James-zr@users.noreply.github.com>
2024-01-05 11:45:30 +0800

    Add files via upload
    新增请求日志切面

---
+++ ResponseRecorder.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+)
+
+// ResponseRecorder 是一个包装了gin.ResponseWriter的结构体，用于记录响应状态码和响应体
+type ResponseRecorder struct {
+	gin.ResponseWriter
+	Body *bytes.Buffer
+}
+
+func NewResponseRecorder(w gin.ResponseWriter) *ResponseRecorder {
+	return &ResponseRecorder{w, &bytes.Buffer{}}
+}
+
+//写入方法`Write`同时将数据写入自己的`Body`缓存和原始的`ResponseWriter`中
+func (r *ResponseRecorder) Write(b []byte) (int, error) {
+	r.Body.Write(b)
+	return r.ResponseWriter.Write(b)
+}
--- ZapLog.go
+++ ZapLog.go
@@ -1,10 +1,15 @@
 package common
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"os"
+	"reflect"
+	"runtime"
+	"time"
 )
 
 var SugarLogger *zap.SugaredLogger
@@ -38,5 +43,100 @@ func getLogWriter(filename string, maxSize, maxBackups, maxAge int, compress boo
 		Compress:   compress, //是否压缩
 	}
 	return zapcore.AddSync(lumberJackLogger)
+}
+
+
+//// HttpLogger 请求日志切面
+//func HttpLogger() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		// 请求的前置处理
+//		start := time.Now()
+//		path := c.Request.URL.Path
+//
+//		// 处理请求
+//		c.Next()
+//
+//		// 请求的后置处理
+//		cost := time.Since(start)
+//		method := c.Request.Method
+//		reqBody := c.Request.Form
+//		clientIP := c.ClientIP()
+//
+//		// 构建日志信息字符串
+//		logInfo := fmt.Sprintf(
+//			"\n请求地址 : %s"+
+//				"\nHTTP METHOD : %s"+
+//				"\n请求参数 : %s"+
+//				"\nIP : %s"+
+//				"\n耗时 : %v",
+//			path, method, reqBody, clientIP, cost,
+//		)
+//
+//		// 使用SugarLogger记录日志信息
+//		SugarLogger.Info(
+//			"\n-------------------请求开始---------------------" +
+//			logInfo +
+//				"\n-------------------请求结束---------------------",
+//			)
+//	}
+//}
+
+
+// HttpLogger 请求日志切面
+func HttpLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 获取请求地址和方法
+		path := c.Request.URL.Path
+		method := c.Request.Method
+		var reqParams interface{}
+
+		// 处理请求前记录时间
+		start := time.Now()
+
+		// 创建响应记录器
+		recorder := NewResponseRecorder(c.Writer)
+		c.Writer = recorder
+
+		// 处理请求
+		c.Next()
+
+		// 请求的后置处理
+		switch c.ContentType() { // 根据 Content-Type 选择合适的方式来获取请求参数
+		case "application/json":
+			var jsonMap map[string]interface{}
+			if err := c.ShouldBindJSON(&jsonMap); err == nil {
+				reqParams = jsonMap
+			}
+		case "application/x-www-form-urlencoded", "multipart/form-data":
+			reqParams = c.Request.Form
+		default:
+			reqParams = nil //其他类型数据暂不做处理
+		}
+
+		cost := time.Since(start)
+		clientIP := c.ClientIP()
+		// 获取 handler 名称
+		handlerName := runtime.FuncForPC(reflect.ValueOf(c.Handler()).Pointer()).Name()
+		// 获取响应数据
+		responseData := recorder.Body.String()
+
+		// 记录所有需要的信息
+		logInfo := fmt.Sprintf(
+			"\nCLASS METHOD: %s"+
+				"\n请求地址: %s"+
+				"\n请求参数 : %s"+
+				"\nHTTP METHOD: %s"+
+				"\nIP: %s"+
+				"\n响应数据: %s"+
+				"\n耗时: %v",
+			handlerName, path, reqParams, method, clientIP, responseData, cost,
+		)
+
+		SugarLogger.Info(
+			"\n---------------------------请求开始-----------------------------" +
+				logInfo +
+				"\n---------------------------请求结束-----------------------------",
+				)
+	}
 }
 
